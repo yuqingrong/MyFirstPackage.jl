b7d9bc98060497d027fe8e8e499de563b6f7dce5
yuqingrong <rongyuqing01@163.com>
2025-03-05 19:42:57 +0800

    modify nv(g)

--- src/shortest_path.jl
+++ src/shortest_path.jl
@@ -1,5 +1,5 @@
 
 function shortest_path(g::AbstractGraph)
     tmat = (map(x->iszero(x) ? zero(TropicalMinPlus{Float64}) : TropicalMinPlus(1.0),
-adjacency_matrix(g)) + Diagonal(fill(TropicalMinPlus(0.0), 10)))^10
+adjacency_matrix(g)) + Diagonal(fill(TropicalMinPlus(0.0), Graphs.nv(g))))^Graphs.nv(g)
 end
--- test/shortest_path.jl
+++ test/shortest_path.jl
@@ -4,6 +4,6 @@ using MyFirstPackage
 @testset "shortest_path" begin
     g = smallgraph(:petersen)
     get_shortest_path = shortest_path(g)
-    dijkstra_shortest_paths_mat = hcat([dijkstra_shortest_paths(g, i).dists for i in 1:nv(g)]...)
+    dijkstra_shortest_paths_mat = hcat([dijkstra_shortest_paths(g, i).dists for i in 1:Graphs.nv(g)]...)
     @test isapprox(getfield.(get_shortest_path, :n), dijkstra_shortest_paths_mat)
 end
